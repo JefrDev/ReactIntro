1d7ef3c1366c7d687753ccb2d720e3f1dada5e45
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-01 12:40:22 +0200

    clear directory for alert excercise

--- ReactIntro/src/components/app/App.tsx
+++ ReactIntro/src/components/app/App.tsx
@@ -1,10 +1,8 @@
-import ButtonType from "../customButton/ButtonType";
-import CustomButton from "../customButton/CustomButton";
+
 
 function App() {
   return (
     <div>
-      <CustomButton buttonType={ButtonType.Primary}>DANGER!!</CustomButton>
     </div>
   );
 }
