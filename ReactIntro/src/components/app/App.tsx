e0b1d5455e5789fea27a8592bd122130430c52da
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-01 11:55:01 +0200

    cleared the directory for the exercise

--- ReactIntro/src/components/app/App.tsx
+++ ReactIntro/src/components/app/App.tsx
@@ -1,22 +1,5 @@
-import Message from "../message/Message";
-import ListGroup from "../listGroup/ListGroup";
-
 function App() {
-  let items = ["bodegraven", "gouda", "delft", "hamburg"];
-
-  const handleOnSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading={"cities"}
-        onSelectItem={handleOnSelectItem}
-      ></ListGroup>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
