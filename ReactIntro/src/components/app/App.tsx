58244b88b076c429502b247206ef735fb24ee905
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-01 13:18:06 +0200

    alert excersize complete

--- ReactIntro/src/components/app/App.tsx
+++ ReactIntro/src/components/app/App.tsx
@@ -1,8 +1,16 @@
-
+import Alert from "../alert/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisibility, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      <Alert
+        onClose={() => setAlertVisibility(false)}
+        active={alertVisibility}
+      ></Alert>
+      <button onClick={() => setAlertVisibility(true)}>Alert!</button>
     </div>
   );
 }
