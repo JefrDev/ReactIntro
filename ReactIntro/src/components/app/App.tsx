05f6149aa8a681ea2fe2322b2fc38b1f48926834
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-01 13:30:35 +0200

    clean directory for testing layouts

--- ReactIntro/src/components/app/App.tsx
+++ ReactIntro/src/components/app/App.tsx
@@ -1,16 +1,10 @@
-import Alert from "../alert/Alert";
+
 import { useState } from "react";
 
 function App() {
-  const [alertVisibility, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      <Alert
-        onClose={() => setAlertVisibility(false)}
-        active={alertVisibility}
-      ></Alert>
-      <button onClick={() => setAlertVisibility(true)}>Alert!</button>
     </div>
   );
 }
